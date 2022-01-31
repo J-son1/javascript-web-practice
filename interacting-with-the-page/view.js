71303e29a6d94789869b788e1cb30b36ba6cd1ac
Jason Humbert <jasonhumbert@outlook.com>
2022-01-31 21:57:57 +0000

    Use JavaScript to dynamically add a new element on the page

--- interacting-with-the-page/view.js
+++ interacting-with-the-page/view.js
@@ -4,6 +4,12 @@ class View {
 
     console.log(this.mainContainerEl);
   }
+
+  addParagraph() {
+    this.newPEl = document.createElement('p');
+    this.newPEl.innerText = 'This paragraph has been dynamically added by JavaScript!';
+    this.mainContainerEl.append(this.newPEl);
+  }
 }
 
 module.exports = View;
