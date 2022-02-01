f6a3f843076b5a8dbfaf2af85aea4c46b609c2ba
Jason Humbert <jasonhumbert@outlook.com>
2022-02-01 14:13:56 +0000

    Add test for View's addParagraph method

--- interacting-with-the-page/view.test.js
+++ interacting-with-the-page/view.test.js
@@ -13,4 +13,14 @@ describe('Page view', () => {
 
     expect(document.querySelectorAll('p').length).toBe(2);
   });
+
+  it('displays 3 paragraphs', () => {
+    document.body.innerHTML = fs.readFileSync('./index.html');
+    
+    const view = new View();
+
+    view.addParagraph();
+
+    expect(document.querySelectorAll('p').length).toBe(3);
+  })
 });
